2084b91ece746dae7ff108891dfc27bc0c35c546
Joris Witteman <joriswitteman@hotmail.com>
2022-12-23 18:58:41 +0100

    Added gerbers generates

---
+++ generate.sh
@@ -0,0 +1,3 @@
+kikit fab jlcpcb --assembly --schematic hardware/motor_pcb/motor_pcb.kicad_sch hardware/motor_pcb/motor_pcb.kicad_pcb ./build/motor
+kikit fab jlcpcb --assembly --schematic hardware/lcd_pcb/lcd_pcb.kicad_sch hardware/lcd_pcb/lcd_pcb.kicad_pcb ./build/lcd
+kikit fab jlcpcb --assembly --schematic hardware/main_pcb/main_pcb.kicad_sch hardware/main_pcb/main_pcb.kicad_pcb ./build/main
